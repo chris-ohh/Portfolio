4ab7bd18b2ca287b6fdce94ae7ba1a53cc448498
zhime <oh2@hawaii.edu>
2019-01-01 23:59:20 -1000

    material ui card example

--- src/components/pages/page.js
+++ src/components/pages/page.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import SimpleCard from './SimpleCard';
 
 class Page extends Component {
   render() {
     return (
       <div className="page">
         <div id="about">About</div>
-        <div id="projects">Projects</div>
+        <div id="projects">
+        <SimpleCard></SimpleCard>
+        <SimpleCard></SimpleCard>
+        <SimpleCard></SimpleCard>
+        <SimpleCard></SimpleCard>
+        Projects</div>
         <div id="education">Education</div>
         <div id="skills">Skills</div>
         <div id="interests">Interests</div>
